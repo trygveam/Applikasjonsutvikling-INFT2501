5f1b21a7657ac8c1fa4126386e61926f6aa0412f
trygveam <trygveam@stud.ntnu.no>
2021-09-01 18:13:11 +0200

    added leksjon2

--- leksjon2/app/src/main/java/inft2501/leksjon2/ChangeFlagActivity.kt
+++ leksjon2/app/src/main/java/inft2501/leksjon2/ChangeFlagActivity.kt
@@ -1,11 +1,34 @@
 package inft2501.leksjon2
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class ChangeFlagActivity : Activity() {
+
+
+    private var flagValue = R.drawable.norway
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_flag)
+        flagValue = intent.getIntExtra("flag", flagValue)
+        val image = findViewById<View>(R.id.imageView) as ImageView
+        image.setImageResource(flagValue)
+    }
+
+    fun onCLickEndActivity(v:View?){
+        setResult(RESULT_OK, Intent().putExtra("flag", flagValue))
+        finish()
+    }
+
+    fun onClickChangeFlag(v:View?){
+        val currentImageIsUK: Boolean = flagValue == R.drawable.uk
+        flagValue = if (currentImageIsUK) R.drawable.norway else R.drawable.uk
+        (findViewById<View>(R.id.imageView) as
+                ImageView).setImageResource(flagValue)
     }
 }
--- leksjon2/app/src/main/java/inft2501/leksjon2/MainActivity.kt
+++ leksjon2/app/src/main/java/inft2501/leksjon2/MainActivity.kt
@@ -1,17 +1,57 @@
 package inft2501.leksjon2
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : Activity() {
+    private val flagRequestCode: Int = 1
+    private var flagValue = R.drawable.norway
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
     fun onClickStartChangeFlagActivity(v: View?) {
         val intent = Intent("inft2501.ChangeFlagActivity")
-        startActivity(intent)
+        intent.putExtra("flag", R.drawable.norway)
+        startActivityForResult(intent, flagRequestCode)
     }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int,
+                                         data: Intent) {
+        if (resultCode != RESULT_OK) {
+            Log.e("onActivityResult()", "Noe gikk galt")
+            return
+        }
+        if (requestCode == flagRequestCode) {
+            flagValue = data.getIntExtra("flag", flagValue)
+            val flag = findViewById<View>(R.id.imageView) as ImageView
+            flag.setImageResource(flagValue)
+        }
+    }
+
+    fun onClickVisAlertDialog(v: View?) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle("AlertDialog")
+        builder.setMessage("Forklarende tekst")
+        builder.setPositiveButton("positiv") { dialog: DialogInterface, which: Int ->
+            Toast.makeText(applicationContext, "positiv", Toast.LENGTH_SHORT).show()
+        }
+        builder.setNegativeButton("negativ") { _, _ ->
+            Toast.makeText(applicationContext, "negativ", Toast.LENGTH_SHORT).show()
+        }
+        builder.setNeutralButton("nøytral") { _, _ ->
+            Toast.makeText(applicationContext, "nøytral", Toast.LENGTH_SHORT).show()
+        }
+        builder.show()
+    }
+
 }
