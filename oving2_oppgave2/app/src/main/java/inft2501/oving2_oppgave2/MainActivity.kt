5bdc67565acb851a9d5a22139bbeb59338f23214
trygveam <trygveamundsen@live.no>
2021-09-02 15:53:13 +0200

    housework and started on oppgave2 in oving2

---
+++ oving2_oppgave2/app/src/main/java/inft2501/oving2_oppgave2/MainActivity.kt
@@ -0,0 +1,11 @@
+package inft2501.oving2_oppgave2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
